ebfcdca070ce701704e46a1c52895f9027e08186
Hwang <feathered.hwang@hotmail.com>
2024-02-28 17:56:53 +0800

    Open KB from config, check after open

--- KBjoint/__init__.py
+++ KBjoint/__init__.py
@@ -3,13 +3,11 @@ import logging
 import os
 from typing import Any
 
-from anki.hooks import addHook
 from aqt import gui_hooks
 from aqt import mw
 # from aqt.qt import *
 from aqt.qt import QFileDialog, QAction, qconnect
-# from aqt.utils import showInfo, askUser
-from aqt.utils import showInfo
+from aqt.utils import showInfo, askUser
 
 from .treejoint import TreeJoint
 
@@ -26,19 +24,12 @@ logging.basicConfig(level=logging.DEBUG,
                     filename=log_file_path,
                     filemode='w')
 # log the module's directory
-logging.info(f'logger file path: {log_file_path}')
-print(f'logger file path: {log_file_path}')
+logging.info(f'Logging file path: {log_file_path}')
 
 # Config set up
 ##################################################
 # legacy type
 ConfigDict = dict[str, Any]
-
-# Path to the configuration file
-CONFIG_JSON_FILE: str = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    f'config@{mw.pm.name.replace(" ", "-")}.json'
-)
 
 config: ConfigDict = {}
 
@@ -47,26 +38,35 @@ def load_json_config():
     """
     Load previous config from json file
     """
-    if os.path.exists(CONFIG_JSON_FILE):
-        with open(CONFIG_JSON_FILE) as f:
-            global config
+    config_json_file: str = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        f'config@{mw.pm.name.replace(" ", "-")}.json'
+    )
+
+    global config
+    if os.path.exists(config_json_file):
+        with open(config_json_file) as f:
             config = json.load(f)
-        logging.info(f'Config File Found: {os.path.basename(CONFIG_JSON_FILE)}\n'
+        logging.info(f'Config File Found: {os.path.basename(config_json_file)}\n'
                      f'The earlier configs: \n{json.dumps(config)}')
-    # logging.info(f'Config File Not Found: {CONFIG_FILEPATH}')
+    else:
+        config = {
+            'config_json_file': config_json_file,
+        }
+        logging.info(f'Config File Not Found: {config_json_file}')
 
 
 def save_json_config():
     """
     Write present config to json file
     """
-    global config
-    with open(CONFIG_JSON_FILE, 'w') as f:
+    with open(config['config_json_file'], 'w') as f:
         f.write(json.dumps(config, indent=4))
 
 
 gui_hooks.profile_did_open.append(load_json_config)
 gui_hooks.profile_will_close.append(save_json_config)
+
 
 # import test modules if exist
 ##################################################
@@ -81,30 +81,24 @@ except ImportError:
 
 def build_models():
     TreeJoint.build_model()
-    logging.info(os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        f'settings@{mw.pm.name.replace(" ", "-")}.json'
-    ))
     # If there are other type of joint/model, put them here
 
 
-# TODO learn more about hook, how many kinds of hook exist
-addHook("profileLoaded", build_models)
+gui_hooks.profile_did_open.append(build_models)
 
 
 # Add menu item
 ##################################################
 
-def kb_join() -> None:
+def kb_join():
     """
     Join your knowledge base to Anki
-    :return: None
     """
     # for read-friendly
     _kb_join()
 
 
-# create a new menu item, "test"
+# create a new menu item
 action = QAction('KB Join', mw)
 # set it to call testFunction when it's clicked
 qconnect(action.triggered, kb_join)
@@ -116,34 +110,19 @@ mw.form.menuTools.addAction(action)
 ##################################################
 
 
-def _kb_join() -> None:
+def _kb_join():
     """
     Join your knowledge base to Anki
     :return: None
     """
 
-    # Set the working directory to the path of current Python script
-    #   used to find template files by absolute path
-    os.chdir(os.path.dirname(os.path.abspath(__file__)))
-    logging.info(f'Current working directory is: {os.getcwd()}')
-
-    # Get KB directory to traverse
-    init_dir = config['last_top_dir'] if config['last_top_dir'] else os.path.expanduser("~")
-    # noinspection PyTypeChecker
-    top_directory = QFileDialog.getExistingDirectory(
-        mw,
-        'Open the Knowledge Base Directory',  # todo cannot understand this calling
-        directory=init_dir
-    )
-    config['last_top_dir'] = top_directory
-
-    # TODO check if KB-folder or not, what if we open a sub-directory of the top-directory
-    # if not askUser("blahblah"):
-    #     pass
-    #     return
-    # else:
-    #     pass
-    logging.info(f'chosen root directory of your knowledge base : {top_directory}')
+    logging.info('================================\n'
+                 'Trying to open KB dir.')
+    top_dir = open_kb_dir()
+    if not top_dir:
+        logging.warning('Opening KB dir cancelled, no cards imported.\n'
+                        '================================================================')
+        return
 
     # TODO leave the file traverse job to joint
     #   and include joint's file analyse - if suitable for this model/joint
@@ -156,14 +135,14 @@ def _kb_join() -> None:
     # mw.progress.finish()
 
     # Traverse the directory tree using os.walk()
-    for root, dirs, files in os.walk(top_directory):
+    for root, dirs, files in os.walk(top_dir):
 
         # Filter out hidden directories and files
         dirs[:] = [d for d in dirs if not d.startswith('.')]
         files = [f for f in files if not f.startswith('.')]
 
         # Calculate the relative path of the current directory
-        relative_root = os.path.relpath(root, top_directory)
+        relative_root = os.path.relpath(root, top_dir)
 
         # replace os.sep with '::' as deck's name
         deck_name = relative_root.replace(os.sep, '::')
@@ -175,9 +154,48 @@ def _kb_join() -> None:
                 logging.debug(f'Inside the directory a md file found: `{file}`')
                 TreeJoint.parse(str(os.path.join(root, file)), str(deck_name))
 
+    logging.warning('__How Many__ notes imported.\n'
+                    '================================================================')
     # todo using message show parse result
-    showInfo('Import succeed. __How Many__')
+    showInfo('__How Many__ notes imported')
 
     # With notes added, refresh the deck browser
     mw.deckBrowser.refresh()
-    # todo refresh the notesBrowser window, show the last added notes
+    # todo open the notesBrowser window, show the last added notes
+
+
+def open_kb_dir() -> str | None:
+    """
+    Get KB directory to traverse
+    :return: The directory path of chosen
+    """
+    try:
+        init_dir = config['last_top_dir']
+    except KeyError:
+        init_dir = os.path.expanduser("~")
+    # noinspection PyTypeChecker
+    top_directory = QFileDialog.getExistingDirectory(
+        mw,
+        'Open the Knowledge Base Directory',
+        directory=init_dir
+    )
+
+    # check if user cancelled selection
+    if not top_directory:
+        logging.info('Opening KB directory cancelled.')
+        return None
+
+    # save config
+    config['last_top_dir'] = top_directory
+
+    # check if KB-folder or not, in case we open a sub of the top-directory
+    if not os.path.exists(os.path.join(top_directory, 'ROOT')):
+        if askUser("The directory you choose do not contain KB 'ROOT' file inside.\n"
+                   "Choose again? "):
+            open_kb_dir()
+        else:
+            logging.info('Opening KB directory cancelled.')
+            return None
+
+    logging.info(f'Chosen root directory of your knowledge base : {top_directory}')
+    return top_directory
