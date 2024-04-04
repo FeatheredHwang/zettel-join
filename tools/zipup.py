51e75554c7b50f0eabe7bef34a663b47181918b6
Hwang <feathered.hwang@hotmail.com>
2024-04-04 15:49:18 +0800

    Feature: Add a script to package up the addon

--- tools/zipup.py
+++ tools/zipup.py
@@ -2,7 +2,9 @@
 Zip up the add-on and giving it a name ending in '.ankiaddon'.
 """
 
+import os
 import shutil
 
 if __name__ == '__main__':
-    shutil.make_archive('KBjoint.ankiaddon', 'zip', '../KBjoint')
+    shutil.make_archive('KBjoint', 'zip', '../KBjoint')
+    os.rename('KBjoint.zip', 'KBjoint.ankiaddon')
