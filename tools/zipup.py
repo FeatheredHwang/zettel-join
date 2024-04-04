1475400fd1a8fda285401e4266cca82f91336671
Hwang <feathered.hwang@hotmail.com>
2024-04-04 17:47:58 +0800

    Fix: delete package created last-time to avoid conflict

--- tools/zipup.py
+++ tools/zipup.py
@@ -5,6 +5,13 @@ Zip up the add-on and giving it a name ending in '.ankiaddon'.
 import os
 import shutil
 
+ADDON_NAME = 'KBjoint'
+ADDON_PACKAGE_NAME = f'{ADDON_NAME}.ankiaddon'
+
+# !IMPORTANT! assign 'TEST_MODE' to False before zipup
+
 if __name__ == '__main__':
-    shutil.make_archive('KBjoint', 'zip', '../KBjoint')
-    os.rename('KBjoint.zip', 'KBjoint.ankiaddon')
+    if os.path.exists(ADDON_PACKAGE_NAME):
+        os.remove(ADDON_PACKAGE_NAME)
+    shutil.make_archive(f'{ADDON_PACKAGE_NAME}', 'zip', f'../{ADDON_NAME}')
+    os.rename(f'{ADDON_PACKAGE_NAME}.zip', f'{ADDON_PACKAGE_NAME}')
