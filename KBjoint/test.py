80d4c35cd5b81877e605b3848d2af761fa35ea02
Hwang <feathered.hwang@hotmail.com>
2024-04-14 10:33:47 +0800

    Code Quality: standardize function names, copy doc to test kasten

--- KBjoint/test.py
+++ KBjoint/test.py
@@ -21,17 +21,25 @@ logging.debug(f'CWD - current working directory: {os.getcwd()}')
 dotenv.load_dotenv()
 
 TEST_MODE: bool = True
-TEST_KB_DIR = os.getenv("TEST_KB_DIR")
+TEST_KB_DIR = os.getenv('TEST_KB_DIR')
+PROJECT_DOC_DIR = os.getenv('PROJECT_DOC_DIR')
 
 
 def reset_test_kb():
     """
     Replace all the test md files in KB, with the original
     """
+    # copy doc files from the project dir
+    for root, dirs, files in os.walk(PROJECT_DOC_DIR):
+        for file in files:
+            # Copy a file, replace if destination file already exist
+            shutil.copy(
+                os.path.join(root, file),
+                os.path.join(TEST_KB_DIR, 'About this addon/doc/.backup', file)
+            )
     for root, dirs, files in os.walk(TEST_KB_DIR):
         if root.endswith('.backup'):
             for file in files:
-                # Copy a file, replace if destination file already exist
                 if file.endswith('.md'):
                     shutil.copy(
                         os.path.join(root, file),
@@ -39,7 +47,7 @@ def reset_test_kb():
                     )
 
 
-def remove_models():
+def remove_test_models():
     """
     remove all the models
     """
@@ -48,7 +56,7 @@ def remove_models():
     mm.remove(mm.id_for_name('Oneside (test)'))
 
 
-def kb_join_test():
+def join_test_kb():
     """
     Join your TEST knowledge base to Anki
     """
@@ -70,17 +78,17 @@ if TEST_MODE:
     gui_hooks.profile_did_open.append(reset_test_kb)
 
     # join test kb as soon as Anki opened
-    gui_hooks.profile_did_open.append(kb_join_test)
-
-    # delete test models before we close Anki
-    gui_hooks.profile_will_close.append(remove_models)
+    gui_hooks.profile_did_open.append(join_test_kb)
 
     # add kb_join_test to the tools menu
     action = QAction('KB Join (test)', mw)
-    qconnect(action.triggered, kb_join_test)
+    qconnect(action.triggered, join_test_kb)
     mw.form.menuTools.addAction(action)
 
     # add output_model to the tools menu
     action = QAction('output Model (test)', mw)
     qconnect(action.triggered, output_model)
     mw.form.menuTools.addAction(action)
+else:
+    # delete test models after we open Anki
+    gui_hooks.profile_did_open.append(remove_test_models)
