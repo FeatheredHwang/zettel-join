ecaf2e330b0f82fc235b217d408ec741e4855d85
Hwang <feathered.hwang@hotmail.com>
2024-04-15 03:11:08 +0800

    Feature: symlink works if run the IDE as administrator

--- tools/symlink.py
+++ tools/symlink.py
@@ -1,18 +1,20 @@
 """
 Create a symbolic link of log file in project dir, pointing to it in addon dir
+[!Note]
+Run the IDE as administrator, then run current script
 """
-# import os
-# from dotenv import load_dotenv
-#
-#
-# load_dotenv()
-#
-# src = os.path.join(os.getenv('ADDON_PATH'), 'root.log')
-# dst = os.path.join(os.getenv('PROJECT_PATH'), '.log')
-# os.symlink(src, dst)
-# print("symlink created")
+import os
+from dotenv import load_dotenv
 
-# it raises "OSError: [WinError 1314] A required privilege is not held by the client"
+
+load_dotenv()
+
+src = os.path.join(os.getenv('ADDON_PATH'), 'root.log')
+dst = os.path.join(os.getenv('PROJECT_PATH'), '.log')
+os.symlink(src, dst)
+print("symlink created")
+
+# it raises "OSError: [WinError 1314] A required privilege is not held by the client" if not run as administrator
 # open powershell as administrator, and execute command below:
 """
 New-Item -ItemType SymbolicLink -Path "D:/Projects/Anki-addon/zettel-join/.log" -Target "C:/Users/feath/AppData/Roaming/Anki2/addons21/zettel-join/root.log" 
