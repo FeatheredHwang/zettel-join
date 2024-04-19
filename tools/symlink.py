59fe98a4ad4a9cde46d7ec1b81aef9e2cb2b3c0d
Hwang <feathered.hwang@hotmail.com>
2024-04-19 20:21:55 +0800

    Feature: introduce reopen IDE as admin while create symlink
    Signed-off-by: Hwang <feathered.hwang@hotmail.com>

--- tools/symlink.py
+++ tools/symlink.py
@@ -12,10 +12,11 @@ load_dotenv('../zettel_join/.env')
 src = os.path.join(os.getenv('ADDON_PATH'), 'DEBUG.log')
 dst = os.path.join(os.getenv('PROJECT_PATH'), 'DEBUG.log')
 os.symlink(src, dst)
-print("symlink created")
+print("DEBUG.log symlink created")
 
 # it raises "OSError: [WinError 1314] A required privilege is not held by the client" if not run as administrator
-# open powershell as administrator, and execute command below:
+# Option 1: reopen IDE as administrator
+# Option 2: open powershell as administrator, and execute command below:
 """
 New-Item -ItemType SymbolicLink -Path "D:/Projects/Anki-addon/zettel-join/.log" -Target "C:/Users/feath/AppData/Roaming/Anki2/addons21/zettel-join/root.log" 
 """
